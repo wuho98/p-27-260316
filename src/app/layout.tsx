97a4cb52a65510d53efeb04a9e6e445a80e45ceb
wuho98 <dngh112@gmail.com>
2026-03-16 12:25:52 +0900

    5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>헤더</h1>
         {children}
       </body>
     </html>
---
+++ src/app/list/page.tsx
@@ -0,0 +1,5 @@
+export default function home() {
+  return (
+      <div>여기는 list 페이지입니다</div>
+  );
+}
