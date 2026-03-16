dff30b920ee09a34d85976081edfc54bf168d13c
wuho98 <dngh112@gmail.com>
2026-03-16 14:31:08 +0900

    7 8 9 10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-red-300`}
       >
         <header>
           <nav>
-            <a href="/">메인</a>
-            <a href="/list">목록</a>
+            <Link href="/">메인</Link>{' '}
+            <Link href="/list">목록</Link>
           </nav>
         </header>
         {children}
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 export default function home() {
   return (
       <div>여기는 list 페이지입니다</div>
